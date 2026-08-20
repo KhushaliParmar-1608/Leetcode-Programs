48509d15022ef6acb7f0c2229205819cdefa4276
KhushaliParmar-1608 <khushaliparmar447@gmail.com>
2026-08-20 19:26:50 +0530

    Time: 3 ms (29.42%), Space: 8.5 MB (83.84%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int reverse(int x) {
+        long rev=0;
+        long rem;
+        while(x!=0){
+            rem =x%10;
+            x=x/10;
+            rev=rev*10+rem;
+        }
+        if(rev>INT_MAX || rev < INT_MIN){
+            return 0;
+        }
+        return rev;
+    }
+};
